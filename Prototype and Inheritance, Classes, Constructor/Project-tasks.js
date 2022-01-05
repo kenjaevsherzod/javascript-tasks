d1e087295a18df9b42acb86859b599f04f89689b
Sherzod Kenjaev <kenjaevsherzod@gmail.com>
2022-01-05 20:59:41 +0500

    Answers for tasks in Prototypes and Inheritance, Classes, Constructor

---
+++ Prototype and Inheritance, Classes, Constructor/Project-tasks.js
@@ -0,0 +1,12 @@
+
+//Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+
+let animal = {
+    move: true
+};
+
+let cat = {
+    __proto__: animal
+};
+
+console.log(cat.move)
