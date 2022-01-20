c6f1c585219b0f7884b37ef62fb507964681a549
Sherzod Kenjaev <kenjaevsherzod@gmail.com>
2022-01-20 14:33:24 +0500

    Answers of third task in Arrays and Cycles

---
+++ arrays_cycles_task_3.js
@@ -0,0 +1,8 @@
+// Given an array of the names of your friends, add the words hello to each element of the array
+
+let friends = ["Abdulaziz", "Jasurbek", "Abdulloh", "Sherzod", "Mohir"];
+
+for (let friend of friends) {
+    let greeting = "Hello " + friend
+    console.log(greeting)
+}
