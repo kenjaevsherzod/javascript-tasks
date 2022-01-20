a625afa5dae0669f982010f1f21362482ed5c03b
Sherzod Kenjaev <kenjaevsherzod@gmail.com>
2022-01-20 14:45:19 +0500

    Answers of seventh task in Arrays and Cycles

---
+++ arrays_cycles_task_7.js
@@ -0,0 +1,7 @@
+// Write a function that takes two parameters - an array and a number and outputs the index of an array element equal to a number
+
+function arrayIndex(arr, num) {
+    return arr[num];
+}
+
+arrayIndex(["goal", "ball", 4, 123, true, false], 2)
