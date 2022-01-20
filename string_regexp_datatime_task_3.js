a87a96d0b8557b3eeafb21372a6ebdde4445a1e6
Sherzod Kenjaev <kenjaevsherzod@gmail.com>
2022-01-20 15:08:56 +0500

    Answers of third task in Strings, RegExp, DataTime

---
+++ string_regexp_datatime_task_3.js
@@ -0,0 +1,7 @@
+// Get the day, month and year of the current date and output it to the console separately
+
+let date = new Date();
+
+console.log("The Year: " + date.getFullYear());
+console.log("The Month: " + date.getMonth());
+console.log("The Day: " + date.getDay());
