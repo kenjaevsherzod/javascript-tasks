2e17060e32e78b1c9d0ffef7a20b1a6a5c30e08f
Sherzod Kenjaev <kenjaevsherzod@gmail.com>
2022-01-20 14:25:51 +0500

    Answers of first task in Arrays and Cycles

---
+++ arrays_cycles_task_1.js
@@ -0,0 +1,7 @@
+// Given an array consisting of movie names, iterate over the array with the output of the names of each movie to the console
+
+let movies = ["Avengers", "Spider-Man", "Matrix", "Kungfu Panda", "Sherlock Holmes"];
+
+for (let movie of movies) {
+    console.log(movie);
+}
